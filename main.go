a1fb3fca412c8d838c3b15f6714ad764f8073694
morida <rida07428@gmail.com>
2025-01-29 08:15:15 +0100

    sba hada

--- main.go
+++ main.go
@@ -16,32 +16,39 @@ func main() {
 		fmt.Println("Read Error.")
 		return
 	}
+	input := os.Args[1]
 
+	input = strings.ReplaceAll(input, "\\n", "\n")
 	lines := strings.Split(string(ascii), "\n")
 	asciiarr := make([][]string, 0)
-	for i := 0; i < len(lines); i += 9 {
-		end := i + 9
+
+	for i := 1; i < len(lines); i += 9 {
+		end := i + 8
 		if end > len(lines) {
 			end = len(lines)
 		}
 		asciiarr = append(asciiarr, lines[i:end])
 	}
 
-	input := os.Args[1]
-	input = strings.ReplaceAll(input, "\\n", "\n")
+	currentLines := [][]string{}
+	line := make([]string, 8)
 
-	output := make([]string, 9)
 	for i := 0; i < len(input); i++ {
-		if input[i] == '\n' {
-			output = append(output, "\n")
-		} else {
-			for j, line := range asciiarr[input[i]-32] {
-				output[j] += line
+		if input[i] != '\n' {
+			for j := 0; j < 8; j++ {
+				line[j] += asciiarr[input[i]-32][j]
 			}
+		} else {
+			currentLines = append(currentLines, line)
+			line = make([]string, 8)
 		}
 	}
 
-	for _, line := range output {
-		fmt.Println(line)
+	currentLines = append(currentLines, line)
+
+	for _, asciiLine := range currentLines {
+		for _, row := range asciiLine {
+			fmt.Println(row)
+		}
 	}
 }
