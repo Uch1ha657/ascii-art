95615dda4b455fd5de2378ead184f9a9ee541dd1
morida <rida07428@gmail.com>
2025-01-30 13:02:41 +0100

    ascii-art is not protected

---
+++ function/Rida.go
@@ -0,0 +1,17 @@
+package function
+
+
+func Rida (input string,asciiarr map[rune][]string) []string {
+	ret := make([]string, 8)
+	for _, v := range input {
+		if ris, booln := asciiarr[v]; booln {
+			for i := 0; i < 8; i++ {
+				ret[i] = ret[i] + ris[i]
+
+			}
+
+		}
+
+	} 
+	return ret	
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"ascii-art/function"
 	"fmt"
 	"os"
 	"strings"
@@ -8,7 +9,7 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("go run . 'input'")
+		fmt.Println("go run . >---> input")
 		return
 	}
 	ascii, err := os.ReadFile("standard.txt")
@@ -19,36 +20,78 @@ func main() {
 	input := os.Args[1]
 
 	input = strings.ReplaceAll(input, "\\n", "\n")
-	lines := strings.Split(string(ascii), "\n")
-	asciiarr := make([][]string, 0)
+	lines := function.Split(string(ascii))
+	asciiarr := make(map[rune][]string)
+	chatr := 32
+	for _, v := range lines {
+		asciiarr[rune(chatr)] = v
+		chatr++
 
-	for i := 1; i < len(lines); i += 9 {
-		end := i + 8
-		if end > len(lines) {
-			end = len(lines)
+	}
+	
+		var rida []string
+		rid := ""
+		ste := false
+
+		if input[len(input)-1] == '\n'{
+			ste = true
 		}
-		asciiarr = append(asciiarr, lines[i:end])
+	
+		for _, x := range input{
+			if x != '\n'{
+				rid += string(x)
+			}else{
+				if rid!= ""{
+					rida = append(rida, rid)
+					rid = ""
+				}
+				if x == '\n'{
+					rida = append(rida, "\n")
+				}
+				
+			}
+		}
+		if rid != ""{
+			rida = append(rida, rid)
+			if ste{
+				rida = append(rida,"\n")
+			}
+		}
+		
+	
+		
+	
+	contr := 0
+	for _, n := range rida {
+		if n == "\n" {
+			contr++
+		}
+
+	}
+	if contr == len(rida) {
+		for contr > 0 {
+			fmt.Println()
+			contr--
+
+		}
+		return
 	}
 
-	currentLines := [][]string{}
-	line := make([]string, 8)
+	for i := 0; i < len(rida); i++ {
+		if rida[i] != "\n" {
+			nrida := function.Rida(rida[i],asciiarr)
+			for i := 0; i < len(nrida); i++ {
+				fmt.Println(nrida[i])
 
-	for i := 0; i < len(input); i++ {
-		if input[i] != '\n' {
-			for j := 0; j < 8; j++ {
-				line[j] += asciiarr[input[i]-32][j]
 			}
 		} else {
-			currentLines = append(currentLines, line)
-			line = make([]string, 8)
+			if i > 1 && i < len(rida)-1 {
+				if !(rida[i-1] != "\n" && rida[i+1] != "\n") {
+					fmt.Println()
+				}
+			}
 		}
+
 	}
 
-	currentLines = append(currentLines, line)
-
-	for _, asciiLine := range currentLines {
-		for _, row := range asciiLine {
-			fmt.Println(row)
-		}
-	}
 }
